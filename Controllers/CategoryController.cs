0bfa17ffa29170fc7f319061ed46f7eaf458fbd2
deepakkumar27J <90575935+deepakkumar27J@users.noreply.github.com>
2024-02-25 22:15:38 +0000

    Interface, Repository, controller, for Category, also 1st api call for getCategories working, also added Mapper

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using expenseTracker.Dto;
+using expenseTracker.Interfaces;
+using expenseTracker.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace expenseTracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController: Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CategoryController(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Category>))]
+        public IActionResult GetCategories()
+        {
+            var categories = _mapper.Map<List<CategoryDto>>(_categoryRepository.GetCategories());
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(categories);
+
+        }
+    }
+}
---
+++ Interfaces/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using expenseTracker.Model;
+
+namespace expenseTracker.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        ICollection<Category> GetCategories();
+        Category GetCategory(int id);
+        ICollection<Expense> GetExpenseByCategory(int categoryId);
+        bool CategoryExists(int id);
+        bool CreateCategory(Category category);
+        bool UpdateCategory(Category category);
+        bool DeleteCategory(Category category);
+        bool Save();
+    }
+}
